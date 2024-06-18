99992474144e6fd60ec7566d31d53741c4bb459a
Cem Akarsubaşı <cemakarsubasi@gmail.com>
2024-06-18 10:38:02 +0200

    Add documentation to typechecker

--- src/crncc/Typechecker.fs
+++ src/crncc/Typechecker.fs
@@ -2,33 +2,34 @@ module CRN.Typechecker
 
 open AST
 
+/// Type errors are a series of strings
 type TypeError = string
 
-// This is the same as the TypingEnv type but also keeps track of dangling references
+/// This is the same as the TypingEnv type but also keeps track of dangling references
 type private TypingEnv' =
     {
-        // All species have to be defined before use and appear on the left side
+        /// All species have to be defined before use and appear on the left side
         Species: Set<string>
-        // Consts can only appear in limited circumstances and species cannot share names with consts
+        /// Consts can only appear in limited circumstances and species cannot share names with consts
         Consts: Set<string>
-        // Dangling
+        /// Dangling values are ones that are used before being previously declared
         Dangling: Set<string>
-        // Mutated
+        /// Values can only be mutated at one part inside a step
         Mutated: Set<string>
     }
 
-// Add a species reference to the environment
+/// Add a species reference to the environment
 let private addreference name env : TypingEnv' =
     {env with Species = env.Species.Add name }
 
-// Resolve a reference to the environment
+/// Resolve a reference to the environment
 let private resolvereference name env = 
     if env.Species.Contains name then
         env
     else
         {env with Dangling = env.Dangling.Add name}
 
-// Check if environment has any dangling species
+/// Check if environment has any dangling species
 let private checkdangling env =
     if env.Dangling.IsSubsetOf env.Species then
         Ok env
@@ -36,13 +37,14 @@ let private checkdangling env =
         let dangling = Set.difference env.Dangling env.Species
         Error [sprintf "Dangling references %O" dangling]
 
+/// Check if something has been previously mutated
 let private checkmutation name env =
     if env.Mutated.Contains name then
         Error [sprintf "Species %s mutated multiple times in step." name]
     else
         Ok {env with Mutated = env.Mutated.Add name}
 
-// left hand side values must be defined
+/// left hand side values must be defined
 let rec private exprtyperleft env expr =
     let rec exprtyperleft' env expr used =
         match expr with 
@@ -56,7 +58,7 @@ let rec private exprtyperleft env expr =
                     exprtyperleft' env tail (Set.add head used)
     exprtyperleft' env expr (Set [])
 
-// right hand side values can be created
+/// right hand side values can be created
 let private exprtyperright env expr =
     let rec exprtyperright' env expr used =
         match expr with 
@@ -70,7 +72,19 @@ let private exprtyperright env expr =
                     exprtyperright' env' tail (Set.add head used)
     exprtyperright' env expr (Set [])
 
-// Modules should not have lhs species appearing in the target
+/// Reaction speed must be above zero, left hand side must not be empty
+let private reactiontyper env reaction =
+    match reaction with
+    (_, _, speed) when speed <= 0.0 -> 
+        Error [sprintf "Reaction speed %f cannot be 0 or below" speed]
+    | ([], _, _)
+    | ([ExprSpecies.Null], _, _) -> Error [sprintf "Attempt at immaculate conception of a species"]
+    | (expr1, expr2, _) -> 
+        exprtyperleft env expr1 
+        |> Result.bind (fun env -> exprtyperright env expr2)
+
+/// Modules should not have lhs species appearing in the target
+/// and target species can only be mutated once
 let private moduletyper env module' =
     match module' with
     | ModuleS.Ld(sp1, target)
@@ -103,7 +117,12 @@ let rec private commandtyper env command =
         | Module(module') -> moduletyper env module'
         | Condition(cond) -> conditiontyper env cond
 
+/// conditions can mutate mutually exclusive things
 and private conditiontyper env condition =
+    // TODO: we still have a problem if we mutate something outside of a condition
+    // in the same step since that is not checked. It might make sense to just reject
+    // all of those programs rather than try to hack my way around this in a more clever
+    // way.
     let rec conditiontyper' env lst =
         match lst with
         [] -> Ok(env)
@@ -120,18 +139,9 @@ and private conditiontyper env condition =
         conditiontyper' env lst
         |> Result.bind (fun env -> Ok {env with Mutated = mutables})
 
-and private reactiontyper env reaction =
-    match reaction with
-    (_, _, speed) when speed <= 0.0 -> 
-        Error [sprintf "Reaction speed %f cannot be 0 or below" speed]
-    | ([], _, _) -> Error [sprintf "Left side of reaction is empty"]
-    | ([ExprSpecies.Null], _, _) -> Error [sprintf "Attempt at immaculate conception of "]
-    | (expr1, expr2, _) -> 
-        exprtyperleft env expr1 
-        |> Result.bind (fun env -> exprtyperright env expr2)
 
+/// Within each step, species can be mutated only once
 let rec private steptyper (env, steps) =
-    // TODO: within the same state, each variable can only be modified once
     let rec steptyper' env steps = 
         match steps with 
         [] -> Ok(env)
@@ -140,6 +150,9 @@ let rec private steptyper (env, steps) =
             |> Result.bind (fun env -> steptyper' env tail)
     steptyper' env steps
 
+/// Concentrations must not include multiple definitions
+/// Right hand side value must be a number or a "constant" that
+/// has to be evaluated before we run the program
 let private conctyper env conc =
     match conc with
     (concName, ValueS.Literal(constName)) -> 
@@ -180,6 +193,9 @@ let rec private crntyper env crn : Result<TypingEnv', TypeError list> =
     Ok env' -> checkdangling env'
     | Error err -> Error err 
 
+/// Typecheck the CRN to ensure that it is valid. Since everything in a CRN is global
+/// This returns the same AST combined with a typing environment that has a list of all
+/// species and constant values that must be evaluated before running the CRN.
 let typecheck untypedast: Result<TypedAST, TypeError list> =
     let env: TypingEnv' = { Species  = Set [] 
                             Consts   = Set [] 
