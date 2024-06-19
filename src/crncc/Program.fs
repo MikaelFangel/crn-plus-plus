5dbd58f04e07f556c850916a22810ae89ae3e0d7
Cem Akarsubaşı <cemakarsubasi@gmail.com>
2024-06-19 15:16:34 +0200

    Fix ODE Solver (#26)

--- src/crncc/Program.fs
+++ src/crncc/Program.fs
@@ -4,21 +4,20 @@ open CRN.Simulator
 open CRN.AST
 
 let [<EntryPoint>] main _ =
-    let initial = Map.ofList ["A", 3.0; "B", 4.0; "C", 0.0]
+    let initial = Map.ofList ["A", 9.0; "B", 0.0]
     let reactions = 
         [ReactionS.Reaction(
             ExprS.Expr(
-                [Species("A")
-                 Species("B")]), 
+                [Species("A")]), 
             ExprS.Expr(
                 [Species("A")
-                 Species("B")
-                 Species("C")]), 1.0)
+                 Species("B")]), 1.0)
          ReactionS.Reaction(
             ExprS.Expr(
-                [Species("C")]
+                [Species("B")
+                 Species("B")]
             ),
-            ExprS.Expr([]), 1.0
+            ExprS.Expr([]), 0.5
          )]
     let states = Seq.take 50 (solveODE initial 0.1 reactions)
     for state in states do
