1dd11560802ae47005129d3f9a5ec6f6cea50af9
Cem Akarsubaşı <cemakarsubasi@gmail.com>
2024-06-19 13:30:53 +0200

    Implement an ODE solver

--- src/crncc/Simulator.fs
+++ src/crncc/Simulator.fs
@@ -1,33 +1,114 @@
 module CRN.Simulator
+
 open CRN.AST
 
-type A = 
-        | Mul of A * A
-        | Add of A * A
-        | Num of float
-        | Var of string
-let rec count x =
-    function
-    | ExprS.Expr [] -> 0
-    | ExprS.Expr (Species y::ys) when x=y -> 1+ (count x (ExprS.Expr ys))
-    | ExprS.Expr (y::ys) -> count x (ExprS.Expr ys)
+/// The state of a system of key value pairs
+/// Each value represents the current concentration of a species
+/// Pass it to an OdeEq to evaluate the current change in the system
+type OdeState = Map<string, float>
 
-let rec mulreac =
-    function 
-    | ExprS.Expr [Species y] -> Var y
-    | ExprS.Expr ((Species y)::ys) -> Mul (Var y, mulreac (ExprS.Expr ys))
-    
-    
-let rec odeof species =
+/// A single ODE Equation
+[<RequireQualifiedAccess>]
+type OdeEq = OdeState -> float
+
+/// A system of ordinary differential equations
+type Ode = { Eqs: Map<string, OdeEq> }
+
+let private getlhs =
     function
-    | [] -> Num 0
-    | ReactionS.Reaction (x,y,k)::xs -> let before = count species x
-                                        let after = count species y
-                                        let change = after-before
-                                        if change = 0 then odeof species xs 
-                                        else let reactants = mulreac y
-                                             Add(Mul (Num ((float change)*k), reactants), odeof species xs) //change*k*reactants+ rest
-let rxnstoodesys rxnsys =
-    let (rxns, env) = rxnsys  
-    Set.fold (fun acc x -> (odeof x rxns)::acc) [] (env.Species)
-    
+    | ReactionS.Reaction(ExprS.Expr(lhs), _, _) -> lhs
+
+let private getrhs =
+    function
+    | ReactionS.Reaction(_, ExprS.Expr(rhs), _) -> rhs
+
+let private getspeed =
+    function
+    | ReactionS.Reaction(_, _, speed) -> speed
+
+let private getname =
+    function
+    | ExprSpecies.Species name -> name
+
+// get all of the unique species
+let private getspecies =
+    function
+    | ReactionS.Reaction(ExprS.Expr(rhs), ExprS.Expr(lhs), _) -> rhs @ lhs |> set
+
+let rec private count x =
+    function
+    | [] -> 0
+    | (Species y :: ys) when x = y -> 1 + (count x ys)
+    | (y :: ys) -> count x ys
+
+let private getchange lhs rhs current =
+    let before = count current lhs
+    let after = count current rhs
+    let change = after - before
+    change
+
+let private composeOde ode1 ode2 =
+    fun m ->
+        let r1 = ode1 m
+        let r2 = ode2 m
+        (r1 + r2)
+
+let private composeOdes (odes: OdeEq list) =
+    List.fold (fun state m -> composeOde state m) (fun _ -> 0.0) odes
+
+let private odeof current lhs rhs speed : OdeEq =
+    let name = getname current
+    let rhsthis = List.filter (fun elem -> elem = current) rhs
+    // change indicates the multiplicity of the current element
+    let change = getchange lhs rhsthis name
+
+    let lhs =
+        List.map
+            (fun r ->
+                match r with
+                | ExprSpecies.Species(s) -> s)
+            lhs
+
+    let ode' (Os: Map<string, float>) =
+        let res =
+            Os
+            |> Map.filter (fun k _ -> List.contains k lhs)
+            |> Map.fold (fun acc _ v -> acc * v) 1.0
+
+        speed * res * (float change)
+
+    ode'
+
+let private createODE (reactions: ReactionS list) : Ode =
+    let m =
+        List.map getspecies reactions
+        |> List.fold Set.union Set.empty
+        |> Set.toList
+        |> List.map (fun one ->
+            let func =
+                reactions
+                |> List.map (fun reaction -> reaction |> getlhs, reaction |> getrhs, reaction |> getspeed)
+                |> List.map (fun (lhs, rhs, speed) -> odeof one lhs rhs speed)
+                |> composeOdes
+
+            getname one, func)
+        |> Map.ofList
+
+    { Eqs = m }
+
+let private forwardEuler system state time =
+    state
+    |> Map.map (fun k v ->
+        let eq = (system.Eqs.Item k)
+        let result = eq state
+        v + time * result)
+
+/// Solve a given ODE based on an initial state and step size
+let solveODE initial step reactions =
+    let h = step
+    let mutable state = initial
+    let ode = createODE reactions
+
+    Seq.initInfinite (fun _ ->
+        state <- forwardEuler ode state h
+        state)
